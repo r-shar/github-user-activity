43107a2626b1e87f4626bc017fd9d7cb5882f531
r-shar <rashmisharma489@gmail.com>
2025-09-20 18:52:08 -0700

    setting up command parser

--- github-user-activity/main.py
+++ github-user-activity/main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+from argparse import ArgumentParser
 
 app = FastAPI()
 
 @app.get("/")
 async def root(): 
   return {"message": "hello world"}
+
+
+
+def main():
+  argparser = ArgumentParser(
+    prog='git-user-activity',
+    description='Lists github user activity',
+  )
+  
+  argparser.add_argument("username")
+  args = argparser.parse_args()
+  print(f'Github username: {args.username}')
+  
+main() 
